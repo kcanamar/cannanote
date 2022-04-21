98ad0eb6bd01163eb3d3f3c9274ed8fa27bac9f8
Kyle Canamar <canamar.kyle@gmail.com>
2022-04-21 13:04:13 -0700

    tests routes created and verified with postman

--- server.js
+++ server.js
@@ -20,8 +20,34 @@ app.use(morgan('tiny'))
 // Declare Routes and Routers 
 ///////////////////////
 // INDUCES - Index, New, Delete, Update, Create, Edit, Show
+// Index
 app.get("/", (req, res) => {
     res.send('It works')
+})
+// New
+app.get("/new", (req, res) => {
+    res.send('Something New')
+})
+// Edit
+app.get("/:id/edit", (req, res) => {
+    res.send(`here is what you asked to edit ${req.params.id}`)
+})
+// Show
+app.get("/:id", (req, res) => {
+    res.send(`Order for ${req.params.id}`)
+})
+// Update
+app.put("/:id", (req, res) => {
+    console.log(`here is what you wanted ${req.params.id}`)
+    res.send(`It is Changed ${req.body}`)
+})
+// Create 
+app.post("/", (req, res) => {
+    res.send(`here is what you made ${req.body}`)
+})
+// Delete
+app.delete("/:id", (req, res) => {
+    res.send(`${req.params.id}....Target neutralized`)
 })
 ///////////////////////////
 // Server Listener
