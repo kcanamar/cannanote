e707ea483160183a31aa561af142afab42dcea2b
Kyle Canamar <canamar.kyle@gmail.com>
2022-04-21 12:43:06 -0700

    establishes working express server on .env PORT and mongoDB connection to collection cannalog

--- server.js
+++ server.js
@@ -1,16 +1,39 @@
 ////////////////////////
 // Setup - Import deps and create app object
 ////////////////////////
-
+require('dotenv').config()
+const express = require('express')
+const methodOverride = require('method-override')
+const mongoose = require('mongoose')
+const morgan = require('morgan')
+const cannaRouter = require('./routes/logs')
+const app = express()
+const PORT = process.env.PORT || 3001
 //////////////////////
 // Declare Middleware
 //////////////////////
-
+app.use(methodOverride('_method'))
+app.use("/static", express.static('public'))
+app.use(express.urlencoded({extended: true}))
+app.use(morgan('tiny'))
 ///////////////////////
 // Declare Routes and Routers 
 ///////////////////////
 // INDUCES - Index, New, Delete, Update, Create, Edit, Show
-
+app.get("/", (req, res) => {
+    res.send('It works')
+})
 ///////////////////////////
 // Server Listener
 ///////////////////////////
+// MongoDB & Mongoose 
+mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+const db = mongoose.connection;
+db.on('error', (err) => console.log(err.message + "yeah... that didn't work"))
+db.on('connected', () => console.log('mongoose connected'))
+db.on('disconnected', () => console.log('mongoose disconnected'))
+
+app.listen(PORT, () => console.log(`Who..Are..You?...${PORT}`))
