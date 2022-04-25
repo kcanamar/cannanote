75e2bdda4c945a90c762e0cc2d516be3737e57e7
Kyle Canamar <canamar.kyle@gmail.com>
2022-04-25 12:51:35 -0700

    creates todo notes

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ middleware(app)
 app.use("/", UnauthRouter)
 // app.use("/", indexRouter)
 app.use("/cannanote", CannaRouter)
-
+// todo not sure where to put this to prevent access to site while not being logged in
 CannaRouter.use((req, res, next) => {
     if (req.session.loggedIn) {
         next()
